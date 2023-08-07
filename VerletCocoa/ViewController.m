798db636c0ac4e3e350253c433cfe076d39fb6a3
Alex Popadich <popadich@gmail.com>
2023-08-07 09:37:49 -0700

    Add actions and outlets for textfield, buttons, and menu items to view controller.

--- VerletCocoa/ViewController.m
+++ VerletCocoa/ViewController.m
@@ -7,7 +7,15 @@
 
 #import "ViewController.h"
 
-@implementation ViewController
+@implementation ViewController {
+    
+    __weak IBOutlet NSTextField *timeLeftField;
+    __weak IBOutlet NSButton *startButton;
+    __weak IBOutlet NSButton *stopButton;
+    __weak IBOutlet NSButton *resetButton;
+    
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -22,5 +30,28 @@
     // Update the view, if already loaded.
 }
 
+
+- (IBAction)startButtonClicked:(NSButton *)sender {
+    [timeLeftField setStringValue:@"Start"];
+}
+- (IBAction)stopButtonClicked:(NSButton *)sender {
+    [timeLeftField setStringValue:@"Stop"];
+}
+- (IBAction)resetButtonClicked:(NSButton *)sender {
+    [timeLeftField setStringValue:@"Reset"];
+}
+
+// these are called because through the first responder
+- (IBAction)startTimerMenuItemSelected:(id)sender {
+    [self startButtonClicked:sender];
+}
+
+- (IBAction)stopTimerMenuItemSelected:(id)sender {
+    [self stopButtonClicked:sender];
+}
+
+- (IBAction)resetTimerMenuItemSelected:(id)sender {
+    [self resetButtonClicked:sender];
+}
 
 @end
