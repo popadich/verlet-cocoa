fe6e084f69fb60660409c29f0665cc4527dee414
Alex Popadich <popadich@gmail.com>
2023-08-07 08:53:06 -0700

    Initial commit

---
+++ VerletCocoa/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  VerletCocoa
+//
+//  Created by Alex Popadich on 8/7/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ VerletCocoa/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  VerletCocoa
+//
+//  Created by Alex Popadich on 8/7/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
